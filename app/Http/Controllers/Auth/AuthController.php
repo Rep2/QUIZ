22c48938818941000958afc494c14059a1a60f4f
Ivan Rep <ivan.rep2@gmail.com>
2015-10-15 22:37:15 +0200

    First commit. Laravel server with jwt authorization

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postRegister(Request $request)
+    {
+        $validator = $this->registerValidator($request->all());
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()->all()],400);
+        }
+
+        Auth::login($this->create($request->all()));
+
+        return $this->postLogin($request);
+    }
+
+    public function postLogin(Request $request)
+    {
+        $validator = $this->loginValidator($request->all());
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()->all()],400);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(compact('token'));
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function registerValidator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6',
+        ]);
+    }
+
+    protected function loginValidator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:6',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+}
