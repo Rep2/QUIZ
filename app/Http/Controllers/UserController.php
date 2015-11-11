f6fc715338ee76802627de1dd170e64015b7fc55
Ivan Rep <ivan.rep2@gmail.com>
2015-11-11 04:04:53 +0100

    Added test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 use Tymon\JWTAuth\Facades\JWTAuth;
